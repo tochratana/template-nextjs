d6a97dcb6183b1545ca536276fcc312168bfdfde
Toch Ratana <tochratana.dev@gmail.com>
2026-06-08 13:47:50 +0700

    fix:sl ss ke

--- src/components/Flowopspage.tsx
+++ src/components/Flowopspage.tsx
@@ -576,7 +576,7 @@ export default function FlowOpsPage() {
           v2.4.0 — Now with AI-driven anomaly detection
         </div>
         <h1>
-          Sl ke mneak eng.
+          sl ss ke
           <br />
           <span className="line2">Chob rean hx.</span>
         </h1>
